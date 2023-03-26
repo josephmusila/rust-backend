27d5a861633f3dea5556a1015151e9dfd26095b2
joseph <josemusila03@gmail.com>
2023-03-26 13:12:05 +0300

    project skeleton

---
+++ target/debug/deps/rust_backend-ac6870a9816c326d.d
@@ -0,0 +1,5 @@
+/home/joseph/Documents/Projects/rustt/rust-backend/target/debug/deps/rust_backend-ac6870a9816c326d.rmeta: src/main.rs
+
+/home/joseph/Documents/Projects/rustt/rust-backend/target/debug/deps/rust_backend-ac6870a9816c326d.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rust_backend-cc0039d307a8de98.d
@@ -0,0 +1,5 @@
+/home/joseph/Documents/Projects/rustt/rust-backend/target/debug/deps/rust_backend-cc0039d307a8de98.rmeta: src/main.rs
+
+/home/joseph/Documents/Projects/rustt/rust-backend/target/debug/deps/rust_backend-cc0039d307a8de98.d: src/main.rs
+
+src/main.rs:
